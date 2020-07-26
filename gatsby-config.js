5a6368d9110859d5ceb0aeeae1b087c7a9bb93d8
Josh Glazer <joshglazer@gmail.com>
2020-07-26 19:22:28 -0400

    purge css

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      },
+    },
+  ],
   siteMetadata: {
     title: `Crush vs. The Orioles`,
     description: `It's 2020 and the baseball season has finally begun. And everyone's wondering, which number will be higher? The amount of wins by The Orioles, or the amount of hits by Crush Davis. Follow along below.`,
