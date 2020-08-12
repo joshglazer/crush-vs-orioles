218a5525173315d06b49b5260b9285e6746b9b50
Josh Glazer <joshglazer@gmail.com>
2020-08-12 18:09:42 -0400

    update standings update gatsby library

--- src/data/player-profile.json.js
+++ src/data/player-profile.json.js
@@ -84,7 +84,7 @@ export const playerProfile = {
                   roe: 0,
                   // Hardcoding with the real number until the API is updated with the current season's stats
                   //   h: 7,
-                  h: 4,
+                  h: 5,
                   cycle: 0,
                 },
                 runs: { total: 7 },
